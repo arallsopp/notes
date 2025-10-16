eba211e515756efce1fc9025446a98cd78613722
Andy Allsopp <arallsopp@gmail.com>
2025-10-16 22:04:24 +0100

    began integrating vexflow

--- script.js
+++ script.js
@@ -45,3 +45,26 @@ function flashKey(keyEl, color) {
         keyEl.style.backgroundColor = originalColor;
     }, 300);
 }
+
+
+/* easy score demo */
+const { Factory, EasyScore, System } = Vex.Flow;
+
+const vf = new Factory({
+    renderer: { elementId: 'stave', width: 500, height: 200 },
+});
+
+const score = vf.EasyScore();
+const system = vf.System();
+
+system
+    .addStave({
+        voices: [
+            score.voice(score.notes('C#5/q, B4, A4, G#4', { stem: 'up' })),
+            score.voice(score.notes('C#4/h, C#4', { stem: 'down' })),
+        ],
+    })
+    .addClef('treble')
+    .addTimeSignature('4/4');
+
+vf.draw();
