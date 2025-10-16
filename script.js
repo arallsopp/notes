bf507b1963be0404458e0e0fcd7a879945d2ca69
Andy Allsopp <arallsopp@gmail.com>
2025-10-16 20:33:04 +0100

    cleaned up script

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ const keys = document.querySelectorAll('.piano-keys');
 
 keys.forEach((key) => {
     key.addEventListener('click', (e) => {
-        let clickedKey = e.target.dataset.key,
-            answer = document.getElementById("answer");
-        answer.innerText = clickedKey;
+        let answer = document.getElementById("answer");
+        answer.innerText = e.target.dataset.key;
     })
 })
