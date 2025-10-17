8ad9e22629d55d144f0bb1196972b9b1344a55e2
Andy Allsopp <arallsopp@gmail.com>
2025-10-17 14:42:18 +0100

    better resizing

--- script.js
+++ script.js
@@ -120,7 +120,7 @@ const div = document.getElementById("stave");
 const renderer = new Renderer(div, Renderer.Backends.SVG);
 
 // Configure the rendering context.
-renderer.resize(500, 200);
+renderer.resize(500, 110);
 const context = renderer.getContext();
 
 function createNote(note) {
@@ -140,7 +140,7 @@ function drawStave() {
     context.clearRect(0, 0, 500, 200);
 
     // Create a stave of width 400 at position 10, 40 on the canvas.
-    const stave = new Stave(10, 40, 400);
+    const stave = new Stave(10, 0, 470);
     let clef = (controls.useBassClef.checked ? "bass" : "treble");
 
     // Add a clef and time signature.
