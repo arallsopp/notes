58b84310a746e681d9542b6ec6774cfec8818b63
Andy Allsopp <arallsopp@gmail.com>
2025-12-13 02:42:55 +0000

    attempt to create offline page

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "timing-helper-v1";
+
+const FILES = [
+    "timings.html",
+    "timings.css",
+    "timings.js",
+    "manifest.webmanifest",
+    "images/icon-192.png",
+    "images/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(r => r || fetch(event.request))
+    );
+});
